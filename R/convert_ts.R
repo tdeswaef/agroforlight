bd068436b16e0cd254be5e1df0b799c3e6e43b95
tdeswaef <tom.deswaef@ilvo.vlaanderen.be>
2024-07-24 16:37:07 +0200

    improved function speed

--- R/convert_ts.R
+++ R/convert_ts.R
@@ -5,7 +5,6 @@
 #' @param conv_factors a tibble as produced by the `make_conv_factors_ts` function
 #' @param treescene_dir_file path to the file of the app output for direct light
 #' @param treescene_diff_file path to the file of the app output for diffuse light
-#' @param start_year the starting year of the agroforestry simulation.
 #' @param lat Latitude of the field in °
 #' @param lon Longitude of the field in °
 #'
@@ -24,10 +23,16 @@
 #' @export
 #'
 #'
-convert_data_ts <- function(conv_factors, treescene_dir_file, treescene_diff_file, start_year, lat, lon){
+convert_data_ts <- function(conv_factors, treescene_dir_file, treescene_diff_file, lat, lon){
 
-  origin <- lubridate::as_date(paste0(start_year, "-12-21"))
+  #1. create solarnoons for the lat and lon for an entire year
+  origin = lubridate::as_date("2019-12-31")
+  solarnoons = tibble::tibble(doy = 1:366,
+                      solarnoon = suncalc::getSunlightTimes(date = as_date(1:366, origin = origin),
+                                                   lat = lat, lon = lon, keep = "solarNoon")$solarNoon) %>%
+    dplyr::mutate(diffnoon = as_datetime((doy*24*3600)+12*3600, origin = origin) - solarnoon)
 
+  #2. read the diff file and pivot_longer
   Diffuse_tree <- data.table::fread(file = treescene_diff_file, sep = ",") %>%
     tidyr::pivot_longer(cols = starts_with("S", ignore.case = F), names_to = c("pos"), values_to = "diff_value") %>%
     tidyr::separate_wider_delim(pos, names = c("discard", "pos"), delim = " ") %>%
@@ -35,21 +40,30 @@ convert_data_ts <- function(conv_factors, treescene_dir_file, treescene_diff_fil
     dplyr::mutate(pos_x = as.numeric(pos_x), pos_y = as.numeric(pos_y)) %>%
     dplyr::select(-discard)
 
-  out <- data.table::fread(file = treescene_dir_file, sep = ",") %>%
-    dplyr::mutate(solarnoon = suncalc::getSunlightTimes(date = as_date(day, origin = origin),
-                                               lat = lat, lon = lon, keep = "solarNoon")$solarNoon,
-                  diffnoon = lubridate::as_datetime(day*(24*3600) + 12*3600, origin = origin) - solarnoon) %>%
-    dplyr::mutate(datetime = lubridate::as_datetime(day*(24*3600) + `time (s)`, origin = origin) - diffnoon) %>%
-    dplyr::inner_join(conv_factors, by = join_by(datetime)) %>%
-    dplyr::select(day, datetime, ConvFactorDirect, ConvFactorDiffuse, starts_with("S", ignore.case = F)) %>%
+  #3.  read the dir file and find the times per day
+  b <- data.table::fread(file = treescene_dir_file, sep = ",")
+  times <- b %>% filter(day == 0) %>% .$`time (s)`
+
+  step1 <- tibble::tibble(doy = conv_factors$datetime %>% yday, year = conv_factors$datetime %>% year) %>% unique %>%
+    dplyr::left_join(solarnoons, by = join_by(doy)) %>%
+    dplyr::group_by(year, doy, diffnoon) %>%
+    dplyr::reframe(`time (s)` = times) %>%
+    dplyr::mutate(datetime = ymd(paste0(year, "-01-01")) + days(doy-1) + seconds(`time (s)`) - diffnoon) %>%
+    dplyr::mutate(day = if_else(doy > 355, doy-356, doy + 10)) %>% select(datetime, day, `time (s)`) %>%
+    dplyr::left_join(b, by = join_by(day, `time (s)`)) %>%
+    dplyr::select(datetime, day, starts_with("S"))
+
+  out <- step1 %>%
     tidyr::pivot_longer(cols = starts_with("S", ignore.case = F), names_to = c("pos"), values_to = "dir_value") %>%
     tidyr::separate_wider_delim(pos, names = c("discard", "pos"), delim = " ") %>%
     tidyr::separate_wider_delim(pos, names = c("pos_x", "pos_y"), delim = "|") %>%
     dplyr::mutate(pos_x = as.numeric(pos_x), pos_y = as.numeric(pos_y)) %>%
+    dplyr::left_join(conv_factors, by = join_by(datetime)) %>%
     dplyr::mutate(direct_rad = dir_value*ConvFactorDirect) %>%
     dplyr::left_join(Diffuse_tree, join_by(day, pos_x, pos_y)) %>%
     dplyr::mutate(diffuse_rad = diff_value*ConvFactorDiffuse) %>%
     dplyr::mutate(total_rad = diffuse_rad + direct_rad) %>%
+    tidyr::drop_na() %>%
     dplyr::select(datetime, pos_x, pos_y, direct_rad, diffuse_rad, total_rad)
 
   return(out)
--- R/makeconv_ts.R
+++ R/makeconv_ts.R
@@ -8,7 +8,6 @@
 #' @param globrad Incoming global radiation in W m-2
 #' @param lat Latitude of the field in °
 #' @param lon Longitude of the field in °
-#' @param start_year starting year of the 3-D time series simulation
 #'
 #' @import readr
 #' @import dplyr
@@ -25,27 +24,43 @@
 #'
 #'
 make_conv_factors_ts <- function(emptyscene_dir_file, emptyscene_diff_file,
-                         datetime, globrad, lat, lon, start_year){
+                         datetime, globrad, lat, lon){
 
+  #0. do an initial check for input data
   if(length(datetime) != length(globrad)) stop("datetime and globrad arguments have unequal length")
-  origin <- as_date(paste0(start_year, "-12-21"))
 
-  DiffuseReference <- data.table::fread(file = emptyscene_diff_file, sep = ",") %>%
-    dplyr::rowwise(day) %>%
-    dplyr::summarise(mean = rowMeans(across(starts_with("S")))) %>% .$mean %>% mean()
+  #1. create solarnoons for the lat and lon for an entire year
+  origin = as_date("2019-12-31")
+  solarnoons = tibble(doy = 1:366,
+                      solarnoon = getSunlightTimes(date = as_date(1:366, origin = origin),
+                                                   lat = lat, lon = lon, keep = "solarNoon")$solarNoon) %>%
+    dplyr::mutate(diffnoon = as_datetime((doy*24*3600)+12*3600, origin = origin) - solarnoon)
 
+  #2. divide the radiation in diff and dir component and make it a function for interpolations
   diffdir_in <- diffdir_fun(datetime = datetime, globrad = globrad, lat = lat, lon = lon)
   diff_fun <- approxfun(x=diffdir_in$datetime, y=diffdir_in$rad_diff)
   dir_fun <- approxfun(x=diffdir_in$datetime, y=diffdir_in$rad_dir)
 
-  out <- data.table::fread(file = emptyscene_dir_file, sep = ",") %>%
-    dplyr::rowwise(day, `time (s)`) %>%
-    dplyr::summarise(DirectReference = rowMeans(across(starts_with("S")))) %>%
-    ungroup() %>%
-    dplyr::mutate(solarnoon = getSunlightTimes(date = as_date(day, origin = origin),
-                                               lat = lat, lon = lon, keep = "solarNoon")$solarNoon,
-                  diffnoon = as_datetime(day*(24*3600) + 12*3600, origin = origin) - solarnoon) %>%
-    dplyr::mutate(datetime = as_datetime(day*(24*3600) + `time (s)`, origin = origin) - diffnoon) %>%
+  #3. read the empty diff file and calculate the ref value
+  DiffuseReference <- data.table::fread(file = emptyscene_diff_file, sep = ",") %>%
+    pivot_longer(cols = starts_with("S")) %>% .$value %>% mean
+
+  #4. read the empty dir file and find the times per day
+  b <- data.table::fread(file = emptyscene_dir_file, sep = ",")
+  times <- b %>% filter(day == 0) %>% .$`time (s)`
+
+  #5. combine all data and return the conversion factors
+  out <- tibble::tibble(doy = datetime %>% yday, year = datetime %>% year) %>% unique %>%
+    dplyr::left_join(solarnoons, by = join_by(doy)) %>%
+    dplyr::group_by(year, doy, diffnoon) %>%
+    dplyr::reframe(`time (s)` = times) %>%
+    dplyr::mutate(datetime = ymd(paste0(year, "-01-01")) + days(doy-1) + seconds(`time (s)`) - diffnoon) %>%
+    dplyr::mutate(day = if_else(doy > 355, doy-356, doy + 10)) %>% select(datetime, day, `time (s)`) %>%
+    dplyr::left_join(b, by = join_by(day, `time (s)`)) %>%
+    dplyr::select(datetime, starts_with("S")) %>%
+    tidyr::pivot_longer(cols = starts_with("S")) %>%
+    dplyr::group_by(datetime) %>%
+    dplyr::reframe(DirectReference = mean(value)) %>%
     dplyr::mutate(diff = diff_fun(datetime), dir = dir_fun(datetime)) %>%
     tidyr::drop_na() %>%
     dplyr::mutate(ConvFactorDiffuse = diff/DiffuseReference,
@@ -54,7 +69,6 @@ make_conv_factors_ts <- function(emptyscene_dir_file, emptyscene_diff_file,
     dplyr::select(datetime, ConvFactorDiffuse, ConvFactorDirect)
 
   return(out)
-
 }
 
 
